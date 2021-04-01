b51cee48a2e071c005b4adc66ea02e52fa440cfa
Jaeung Kim <jaeung949@gmail.com>
2021-04-01 15:57:32 +0900

    init

---
+++ src/test/java/com/ca/homefinder/HomeFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ca.homefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
