9fea2d66afe32294ef5dc0d42a59555054836158
Jaeung Kim <jaeung949@gmail.com>
2021-04-24 22:22:53 +0900

    Update APIController.java

--- src/main/java/hf/controller/APIController.java
+++ src/main/java/hf/controller/APIController.java
@@ -1,7 +1,7 @@
 package hf.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.json.JsonParser;
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +15,13 @@ public class APIController {
     @PostMapping("/postTest")
     public Object postForEntity(@RequestBody Map contents){
         RestTemplate restTemplate = new RestTemplate();
-
-//        JsonParser parser;
-//        parser.parseMap()
-
-
-        Object response = restTemplate.postForObject("http://localhost:9200/officetel-rent-data/_search", contents, Map.class);
+        String queryInjector = "";
+        queryInjector = (String) contents.get("city");
+        String query = "{\"size\":0,\"query\":{\"bool\":{\"must\":[{\"prefix\":{\"시군구\":\"" +
+                queryInjector +
+                "\"}}]}},\"aggs\":{\"result\":{\"date_histogram\":{\"field\":\"@timestamp\",\"format\":\"yyyy-MM\",\"calendar_interval\":\"month\"},\"aggs\":{\"보증금\":{\"avg\":{\"field\":\"deposit\"}},\"면적\":{\"avg\":{\"field\":\"area\"}},\"translated\":{\"bucket_script\":{\"buckets_path\":{\"v1\":\"면적\",\"v2\":\"월세\",\"v3\":\"보증금\"},\"script\":\"(params.v2 + (params.v3 * 0.05 /12)) / params.v1 * 33\"}},\"월세\":{\"avg\":{\"field\":\"rent\"}}}}}}\n";
+        JSONObject queryObject = new JSONObject(query);
+        Object response = restTemplate.postForObject("http://localhost:9200/officetel-rent-data/_search", queryObject.toMap(), Map.class);
         return response;
     }
 }
